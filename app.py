62ec57ab80c274f1d85f6704f78fdf8289a770d5
Ankita Sharma <ankitasharma.281993@gmail.com>
2023-05-06 00:04:10 +0530

    made changes to page style

--- app.py
+++ app.py
@@ -18,15 +18,12 @@ def create_app():
     def add_update_list_method():
         def update_article_list():
             article_count = app.db.entries.count_documents({})
-            print(article_count)
             if article_count != len(entries):
                 for en in app.db.entries.find({}):
                     entries.append(Blog_Entry.db_map_entry(en))
-                return True
-            else:
-                return False
+            return ""
         return {"update_article_list" : update_article_list}
-    
+
     @app.route("/", methods=["GET", "POST"])
     def main_page():
         if request.method == "POST":
@@ -42,5 +39,26 @@ def create_app():
             entries.append(entry)
             app.db.entries.insert_one(entry.db_insert_entry())
         return render_template("index.html", entries=entries)
+    
+    @app.route("/add", methods=["GET", "POST"])
+    def article_page():
+        if request.method == "POST":
+            entry_title = request.form.get("title")
+            entry_author = request.form.get("author")
+            entry_image = request.form.get("image")
+            entry_image_alt = request.form.get("image_alt")
+            entry_content = request.form.get("content")
+            formatted_date = datetime.datetime.today()
+            entry = Blog_Entry(entry_title,
+                            entry_author, 
+                            formatted_date.strftime("%Y-%m-%d"), 
+                            formatted_date.strftime("%b %d"),
+                            entry_image,
+                            entry_image_alt,
+                            entry_content
+                            )
+            entries.append(entry)
+            app.db.entries.insert_one(entry.db_insert_entry())
+        return render_template("add_article.html", entries=entries)
     
     return app
--- model/blog_entry.py
+++ model/blog_entry.py
@@ -3,46 +3,61 @@ import datetime
 class Blog_Entry:
     def __init__(self,
                  title,
-                 content, 
-                 article_date, 
-                 formatted_article_date,
-                 createdAt) -> None:
+                 author,
+                 published_date,
+                 formatted_date,
+                 image,
+                 image_alt,
+                 content) -> None:
         self.__title = title
+        self.__author = author
+        self.__published_date = published_date
+        self.__formatted_date = formatted_date
+        self.__image = image
+        self.__image_alt = image_alt
         self.__content = content
-        self.__article_date = article_date
-        self.__formatted_article_date = formatted_article_date
-        self.__createdAt = createdAt
+        
 
     def get_title(self) -> str:
         return self.__title
+    def get_author(self) -> str:
+        return self.__author
+    def get_publisheddate(self) -> str:
+        return self.__published_date
+    def get_formatteddate(self) -> str:
+        return self.__formatted_date
+    def get_image(self) -> str:
+        return self.__image
+    def get_image_alt(self) -> str:
+        return self.__image_alt
     def get_content(self) -> str:
         return self.__content
-    def get_publishdate(self) -> str:
-        return self.__article_date
-    def get_formatted_publishdate(self) -> str:
-        return self.__formatted_article_date
-    def get_createdAt(self) -> datetime:
-        return self.__createdAt
     
     def db_insert_entry(self) -> dict:
         return {
             "title" : self.__title,
+            "author" : self.__author,
+            "published_date" : self.__published_date,
             "content" : self.__content,
-            "publish_date" : self.__article_date,
-            "formatted_article_date" : self.__formatted_article_date,
-            "createdAt" : self.__createdAt
+            "image_link" : self.__image,
+            "image_alt" : self.__image_alt
         }
     
     @staticmethod
     def db_map_entry(entry_json):
+        pub_date = datetime.datetime.strptime(entry_json.get("published_date"), "%Y-%m-%d").date()
+        formatted_date = pub_date.strftime("%b %d")
+
         return Blog_Entry(
             entry_json.get("title"),
-            entry_json.get("content"),
-            entry_json.get("publish_date"),
-            entry_json.get("formatted_article_date"),
-            entry_json.get("createdAt")
+            entry_json.get("author"),
+            entry_json.get("published_date"),
+            formatted_date,
+            entry_json.get("image_link"),
+            entry_json.get("image_alt"),
+            entry_json.get("content")
         )
 
     def __str__(self) -> str:
-        return self.__content +" "+self.__article_date
+        return self.__title +" "+self.__author
     
