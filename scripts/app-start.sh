97685186ce3f6d23976b8dfd90dc3ab9e7917f20
Ankita Sharma <ankitasharma.281993@gmail.com>
2023-07-03 12:55:09 +0530

    scripts change

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,1 +1,3 @@
-/home/ec2/app/app_env/Scripts/gunicorn "/home/ec2/app/blogging:create_app()"
+cd /home/ec2-user/app
+source app_env/bin/activate
+/home/ec2-user/app-user/app_env/bin/gunicorn --bind 0.0.0.0:5000 -m 007 "blogging:create_app()"
--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,6 +1,4 @@
-rm -r application
-mkdir application
-cd application
+cd /home/ec2-user/app
 python3 -m venv app_env
 source app_env/bin/activate
 pip3 install -r /home/ec2-user/app/requirements.txt
