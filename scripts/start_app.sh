d9a9dce5f8e4a3cfb6ce6774a0cfc0a9edf0ae61
Ankita Sharma <ankitasharma.281993@gmail.com>
2023-07-03 23:15:34 +0530

    changes for gunicorn deploy

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo systemctl daemon-reload
+sudo systemctl enable --now gunicorn.socket
 sudo systemctl start gunicorn.service    # starts up the service
 sudo systemctl status gunicorn.service   # prints the status to the log
