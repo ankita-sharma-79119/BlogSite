887e4d4a86f1b11655511e0fef7f95e5e563d41d
Ankita Sharma <ankitasharma.281993@gmail.com>
2023-07-03 13:04:25 +0530

    scripts change

--- scripts/app-start.sh
+++ scripts/app-start.sh
@@ -1,3 +1,3 @@
 cd /home/ec2-user/app
 source app_env/bin/activate
-/home/ec2-user/app-user/app_env/bin/gunicorn --bind 0.0.0.0:5000 -m 007 "blogging:create_app()"
+gunicorn --bind 0.0.0.0:5000 -m 007 "blogging:create_app()"
