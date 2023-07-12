86909939f9e28a3329b8db59b3be3607884e84d1
Ankita Sharma <ankitasharma.281993@gmail.com>
2023-07-12 22:42:23 +0530

    change few commands order of execution

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -1,2 +1,13 @@
+# Install dependencies
 source /env/bin/activate
 pip3 install -r /home/ec2-user/app/requirements.txt
+
+# Configure supervisor
+sudo cp /home/ec2-user/techwired/scripts/gunicorn.conf /etc/supervisor/conf.d/
+sudo supervisorctl reread
+sudo supervisorctl update
+
+# Configure nginx
+sudo cp /home/ec2-user/techwired/scripts/flask.conf /etc/nginx/sites-available/
+sudo cp /home/ec2-user/techwired/scripts/flask.conf /etc/nginx/sites-enabled/
+sudo service nginx restart
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -7,13 +7,18 @@ sudo apt-get install -y nginx
 pip install gunicorn
 sudo apt-get install supervisor
 
-sudo cp /home/ec2-user/techwired/scripts/gunicorn.conf /etc/supervisor/conf.d/
+# Configure supervisor configs
 sudo mkdir /etc/supervisor/conf.d/var/log/gunicorn
 sudo supervisorctl reread
 sudo supervisorctl update
 
 # change user to root in nginx.conf file in etc/nginx
 
-sudo cp /home/ec2-user/techwired/scripts/flask.conf /etc/nginx/sites-available/
-sudo cp /home/ec2-user/techwired/scripts/flask.conf /etc/nginx/sites-enabled/
-sudo service nginx restart
+# Configuring nginx
+# sudo cp /home/ec2-user/techwired/scripts/flask.conf /etc/nginx/sites-available/
+# sudo cp /home/ec2-user/techwired/scripts/flask.conf /etc/nginx/sites-enabled/
+# sudo service nginx restart
+
+
+# Configure supervisor
+# sudo cp /home/ec2-user/techwired/scripts/gunicorn.conf /etc/supervisor/conf.d/
